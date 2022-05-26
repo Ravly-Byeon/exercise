6754d4815f06a3b503356866edf4c1335e3ebec4
YR BYEON <ravly.b.4702@gmail.com>
2022-05-26 10:02:50 +0900

    vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 import VueAxios from "vue-axios";
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
@@ -11,5 +12,6 @@ Vue.use(VueAxios, axios);
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
